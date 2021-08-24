5dc9906fe138fa496355ae8bc099b50ca221dba1
Vinicius Gutierrez <srgutyerrez@gmail.com>
2021-08-24 06:17:02 -0300

    dev

--- server.ts
+++ server.ts
@@ -1,13 +1,15 @@
-import { Ignitor } from '@adonisjs/core/build/standalone'
-
 import 'reflect-metadata'
+
+import mercadopago from 'mercadopago'
+
+import { Ignitor } from '@adonisjs/core/build/standalone'
 
 // Configure payment gateways
 
-// mercadopago.configure({
-//   client_id: process.env.MERCADO_PAGO_CLIENT_ID!!,
-//   client_secret: process.env.MERCADO_PAGO_CLIENT_SECRET!!,
-// })
+mercadopago.configure({
+  client_id: '7934218825408920',
+  client_secret: 'GsL793YiuuoFGvGHUOfWo35q68acv2Ve',
+})
 
 // Create instance of ignitor
 
